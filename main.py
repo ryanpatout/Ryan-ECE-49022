990f773737d5d5f6ec0ca6401e70394299e89441
Ryan Patout <Ryan_Patout@pal-nat186-184-71.itap.purdue.edu>
2020-03-12 09:10:54 -0400

    rev2

--- main.py
+++ main.py
@@ -3,19 +3,25 @@
 # Senior Design Team 11
 # Load Cell Data
 
+import math
+
 def main():
-    dataSet = {}
-    filenames = ['percentiles']
-    for file in filenames:
+    dataSet = {}                            # setting an empty dictionary
+    filenames = ['percentiles']             # referencing the data filenames
+    for file in filenames:                  # loop to get data
         data = getData(file)
-        data = [float(d) for d in data]
         dataSet[file] = data
     print(dataSet)
+    
 
 def getData(file):
-    myFile = open(file + ".txt", 'r')
-    data = myFile.readlines()
-    myFile.close()
+    myFile = open(file + ".txt", 'r')       # open in read mode
+    data = myFile.readlines()               # read lines into list
+    myFile.close()                          # close file
+    data = [float(d) for d in data]         # convert data to float
+    data = [(v/3*4096) for v in data]       # convert to ADC values
+    data = [round(x, 3) for x in data]      # set precision of floats
     return data
+
 
 main()
