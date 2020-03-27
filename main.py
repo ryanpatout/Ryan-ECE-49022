ac66f7865dfe0ef34df97c0a244380a85c7bf12a
Ryan Patout <Ryan_Patout@Ryans-MacBook-Air-2.local>
2020-03-27 19:13:51 -0400

    final rev

--- main.py
+++ main.py
@@ -3,22 +3,25 @@
 # Senior Design Team 11
 # Load Cell Data
 
-from sklearn import linear_model
-from sklearn.metrics import r2_score
-
+import numpy as np
 
 def main():
-    x_train = [100.0, 90.0, 80.0, 70.0, 60.0, 50.0, 40.0, 30.0, 20.0, 10.0, 0.0]
-    dataSet = {}                                                # setting an empty dictionary
-    filenames = ['bag2_test1']                                  # referencing the data filenames
+    dataSet=[]
+    percentages = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+    perc = [float(d) for d in percentages]
+    filenames = ['bag1_test1', 'bag1_test2']                    # referencing the data filenames
     for file in filenames:                                      # loop to get data
         data = getData(file)                                    # returns data in list form
-        dataSet[file] = data                                    # adds data with name to dictionary
-    y_train = []                                                # initializing empty list for y's
-    for data in dataSet.values():
-        y_train.append(data)                                    # append all y values
-    regression(x_train, y_train)                                # sending data to regression
-    
+        dataSet.append(data)
+    test1, test2 = dataSet
+    test1 = test1[::-1]
+    test2 = test2[::-1]
+    a = 0.0274                                                  # coefficient of line of best fit
+    b = 0.2924                                                  # coefficient of line of best fit
+    predVout = predictedVal(perc, a, b)
+    actVout = average(test1, test2)
+    MSE = meanSquared(predVout, actVout)
+    print('MSE:', MSE)
 
 def getData(file):
     myFile = open(file + ".txt", 'r')                           # open in read mode
@@ -30,16 +33,37 @@ def getData(file):
     return data
 
 
-def regression(x_train, y_train):
-    x_test = [float(i) for i in range(101)]
-    regr = linear_model.LinearRegression(fit_intercept=True)        # define lin reg object
-    regr.fit(x_train, y_train)                                      # fit model to training set
-    coefs = regr.coef_                                              # tuple of coefficients
-    print(coefs)
-    intc = regr.intercept_                                          # b intercept
-    print(intc)
-    y_pred = regr.predict(x_test)                                   # apply model to test
-    # r2 = r2_score(y_true, y_pred)
+def predictedVal(perc, a, b):
+    Vout = []
+    for x in perc:
+        temp = a * x + b
+        Vout.append(temp)
+    Vout = [(v / 3 * 4096) for v in Vout]  # convert to ADC values
+    Vout = [round(x, 3) for x in Vout]  # set precision of floats
+    return Vout
+
+
+def average(test1, test2):
+    avg = []
+    index = 0
+    while index <= 20:
+        temp = (test1[index]+test2[index])/2
+        avg.append(temp)
+        index += 1
+        with open('predicted.txt', 'a') as file:                    # writing to a new file to store average values
+            file.write(str(temp)+'\n')
+    return avg
+
+
+def meanSquared(pred, act):
+    index = 0
+    MSE = 0
+    while index <= 20:
+        squared = (act[index]-pred[index])**2
+        MSE += squared
+        index += 1
+    MSE /= len(act)
+    return MSE
 
 
 main()
